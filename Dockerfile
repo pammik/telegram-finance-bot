0ade5d82d2e39b90747b366c96c0d628c631cfa2
pamik <pamik@list.ru>
2021-04-10 23:25:08 +0300

    test

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /home
 
 ENV TELEGRAM_API_TOKEN=""
 ENV TELEGRAM_ACCESS_ID=""
+ENV TELEGRAM_ACCESS_ID_1=""
 
 ENV TZ=Europe/Moscow
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
--- server.py
+++ server.py
@@ -15,10 +15,12 @@ logging.basicConfig(level=logging.INFO)
 
 API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 ACCESS_ID = os.getenv("TELEGRAM_ACCESS_ID")
+ACCESS_ID_1 = os.getenv("TELEGRAM_ACCESS_ID_1")
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(AccessMiddleware(ACCESS_ID))
+dp.middleware.setup(AccessMiddleware(ACCESS_ID_1))
 
 
 @dp.message_handler(commands=['start', 'help'])
